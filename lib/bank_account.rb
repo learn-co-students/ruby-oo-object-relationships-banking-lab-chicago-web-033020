cca5e271d0d8fabec731aacb1fc83c08af37f577
gozmeister <samgorick.sg@gmail.com>
2020-04-02 18:21:16 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+require 'pry'
+#require_relative 'transfer.rb'
+
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    self.class.all << self
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 
 end
+
